3286e7387c56e169953e5cdcdfd3b2d148d4448c
Carlos Zúñiga <carlos.zun@gmail.com>
2012-02-11 19:26:16 -0500

    Added ground movement

--- main.lua
+++ main.lua
@@ -1,33 +1,46 @@
 
 conf = {}
 conf.gravity = 320
-conf.sideForce = 800
+conf.sideForce = 1200
 conf.upForce = {}
 conf.upForce[0.1] = -2000
 conf.upForce[0.4] = -5000
 conf.upForce[0.7] = -7000
+conf.meterScale = 30 -- 30 pixels = 1 meter
 
 controls = {}
 controls.flap = 'z'
-controls.left = 'j'
-controls.right = 'l'
+controls.left = 'left'
+controls.right = 'right'
 
 function love.load()
-    world = love.physics.newWorld(0,0,640,480)
+    world = love.physics.newWorld(0, 0, 640, 480)
     world:setGravity(0, conf.gravity)
-    world:setMeter(64)
+    world:setMeter(conf.meterScale) -- default
+    world:setCallbacks(add, persist, rem, result)
 
     objects = {}
 
     objects.ground = {}
     objects.ground.body = love.physics.newBody(world, 640/2, 440, 0, 0)
     objects.ground.shape = love.physics.newRectangleShape(objects.ground.body, 0, 0, 480, 80)
+    objects.ground.shape:setData("ground")
 
     objects.ball = {}
     objects.ball.body = love.physics.newBody(world, 640/2, 480/2, 15, 0)
     objects.ball.shape = love.physics.newCircleShape(objects.ball.body, 0, 0, 20)
     objects.ball.lastFlap = 0
+    objects.ball.shape:setData("ball")
+    objects.ball.groundVelocity = 300
+    objects.ball.inGround = false
+
+    objects.bubble = {}
+    objects.bubble.body = love.physics.newBody(world, 640/2, 480/2, 0, 0)
+    objects.bubble.shape = love.physics.newCircleShape(objects.ball.body, 0, 0, 20)
+    objects.bubble.shape:setData("bubble")
+
     elapsedTime = 0
+    text = ""
 
     love.graphics.setBackgroundColor(104, 136, 248)
     love.graphics.setMode(640, 480, false, true, 0)
@@ -35,6 +48,14 @@ end
 
 function love.update(dt)
     world:update(dt)
+    objects.bubble.body:applyForce(0,-1500)
+    if objects.ball.inGround then
+        if love.keyboard.isDown(controls.right) then
+            objects.ball.body:applyForce(objects.ball.groundVelocity, 0)
+        elseif love.keyboard.isDown(controls.left) then
+            objects.ball.body:applyForce(-objects.ball.groundVelocity, 0)
+        end
+    end
 end
 
 function love.draw()
@@ -48,7 +69,17 @@ function love.draw()
         objects.ball.body:getY(),
         objects.ball.shape:getRadius(), 20
     )
+
+    love.graphics.setColor(47, 47, 140)
+    love.graphics.circle(
+        "line", 
+        objects.bubble.body:getX(),
+        objects.bubble.body:getY(),
+        objects.bubble.shape:getRadius(), 20
+    )
+
     love.graphics.print(string.format("elapsedTime: %s", elapsedTime), 0, 0)
+    love.graphics.print(text, 0, 15)
 end
 
 function flap()
@@ -85,3 +116,24 @@ function love.keypressed(k)
   --if k=='d' then DEBUG=not DEBUG end
 end
 
+function add(a, b, coll)
+    if a == "ground" and b == "ball" then
+        objects.ball.inGround = true
+        text = text.. "add " .. a .. " : " .. b .. "\n"
+    end
+end
+
+function persist(a, b, coll)
+    
+end
+
+function rem(a, b, coll)
+    if a == "ground" and b == "ball" then
+        objects.ball.inGround = false
+        text = text.. "rem " .. a .. " : " .. b .. "\n"
+    end
+end
+
+function result(a, b, coll)
+    
+end
