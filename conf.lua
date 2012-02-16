9f41addb09c29c221ccf4bb637141b3e91e247a4
Carlos Zúñiga <carlos.zun@gmail.com>
2012-02-16 00:30:57 -0500

    Added stuff to conf.lua

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.author   = "carlosz, ejosvp & ratasxy"
+    t.identity = "ballonfight"
+
+    t.screen.width  = 1024
+    t.screen.height = 600
+    t.screen.fullscreen = true
+
+    t.modules.joystick = false
+end
